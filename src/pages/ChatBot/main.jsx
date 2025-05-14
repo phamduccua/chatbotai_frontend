e114c1a582e85b7778fdbbe23650a3a2040bff03
trantrien <zuka5467@gmail.com>
2025-05-14 22:22:43 +0700

    Update main.jsx

--- src/pages/ChatBot/main.jsx
+++ src/pages/ChatBot/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import './ChatBot.css'
 import ChatBot from './ChatBot.jsx'
 
 createRoot(document.getElementById('root')).render(
