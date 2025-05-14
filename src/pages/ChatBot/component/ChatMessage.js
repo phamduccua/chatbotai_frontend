61969097f99c53bd01a350ca6cdbdc0ad43d6d9c
trantrien <zuka5467@gmail.com>
2025-05-14 17:28:07 +0700

    Update ChatMessage.js

--- src/pages/ChatBot/component/ChatMessage.js
+++ src/pages/ChatBot/component/ChatMessage.js
@@ -1,12 +1,21 @@
-import ChatbotIcon from "./ChatbotIcon";
-function ChatMessage({chat}){
+import ChatbotIcon from "./Chatboticon";
+
+const ChatMessage = ({chat}) => {
     return (
-        <div className={`message ${chat.role == "model" ? 'bot' : 'user'}-message`}>
+        <div className={`message ${chat.role === "model" ? 'bot' : 'user'}-message`}>
             {chat.role === "model" && <ChatbotIcon />}
             <div className="message-text">
                 {chat.text}
+                {chat.file && (
+                    <img 
+                        src={`data:${chat.file.mime_type};base64,${chat.file.data}`}
+                        alt="attachment"
+                        className="attachment"
+                    />
+                )}
             </div>
         </div>
     );
 };
+
 export default ChatMessage;
